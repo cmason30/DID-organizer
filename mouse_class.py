75dbb83211c1a6a30521cbfee627a9b1c369c29f
raceee <race.peterson@gmail.com>
2019-11-07 20:25:38 -0700

    house created

--- mouse_class.py
+++ mouse_class.py
@@ -2,6 +2,9 @@ class Mouse:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
+    def __del__(self):
+        print("Mouse deleted, dead in real life and dead from our memory.")
+
     @staticmethod
     def data_dict():
         empty_dict = dict.fromkeys(['number', 'weight', 'D1VI', 'D2VI', 'D3VI', 'D4VI', 'D1VF', 'D2VF', 'D3VF', 'D4VF',
@@ -15,7 +18,8 @@ class Mouse:
         return mouse_data
 
 choice = 0
-#TODO: read saved mouse data
+mouse_house = []
+#TODO: read saved mouse data from csv
 while choice != -1:
     print("Hello and welcome to the Mouse House of DID(TM). Here we will keep the data of our mice. Enter the correspoding "
           "number to the task that you would like to do and enter -1 to quit this program")
@@ -23,18 +27,27 @@ while choice != -1:
     print("2- Would you like to delete a mouse?") #by number so conditional check on number
     print("3- Would you like to export the Mouse House of Data(TM) to a .csv?")
     print("4- Would you like to enter Nightmare mode?")
-
     print("5- View the tennants of the current Mouse House?") #The Mouse House of Data(TM) happily homes these mice:
     choice = int(input("What option would you like? "))
     print(" ")
 
     if choice == 1:
-        Mouse.data_dict()
-    # if choice == 2:
+        data = Mouse.data_dict()
+        mouse = Mouse(**data)
+        mouse_house.append(mouse)
+
+    if choice == 2:
+        mouseID = float(input("Enter the number of the mouse you would like to delete. "))
+        for item in mouse_house:
+            if item.number == mouseID:
+                del item
+                print(item)
+
+
+
     # if choice == 3:
     # if choice == 4:
     # if choice == 5:
 #TODO: api that saves mouse house to master_csv
 #TODO: api that reads master_csv and instantiates a mouse house.
 
-print(data_dict())
