759f8295111f4751fe2fd94a4e7171395afdd641
Colin Mason <colinpetermason@gmail.com>
2019-12-09 12:43:00 -0700

    working databse and UI

--- backend_did.py
+++ backend_did.py
@@ -1,179 +1,29 @@
 import sqlite3
 from tkinter import *
-# import frontend_did
-
-# def connect():
-#     conn = sqlite3.connect("mouse_house.db")
-#     cur = conn.cursor()
-#     cur.execute("CREATE TABLE IF NOT EXISTS mouse_house2(id INTEGER PRIMARY KEY, 'mouse number' text, weight float)")
-#     conn.commit()
-#     conn.close()
 
 
-class DCMod:
-
-    def __init__(self):
-        win2 = Tk()
-
-        lab1 = Label(win2, text="Day 1")
-        lab1.grid(row=0, column=0)
-
-        lab2 = Label(win2, text="Day 1 DC1 Initial")
-        lab2.grid(row=1, column=0)
-
-        lab3 = Label(win2, text="Day 1 DC1 Final")
-        lab3.grid(row=1, column=2)
-
-        lab4 = Label(win2, text="Day 1 DC2 Initial")
-        lab4.grid(row=2, column=0)
-
-        lab5 = Label(win2, text="Day 1 DC2 Final")
-        lab5.grid(row=2, column=2)
-
-        lab6 = Label(win2, text="Day 2 DC1 Initial")
-        lab6.grid(row=4, column=0)
-
-        lab7 = Label(win2, text="Day 2 DC1 Final")
-        lab7.grid(row=4, column=2)
-
-        lab8 = Label(win2, text="Day 2 DC2 Initial")
-        lab8.grid(row=5, column=0)
-
-        lab9 = Label(win2, text="Day 2 DC2 Final")
-        lab9.grid(row=5, column=2)
-
-        lab10 = Label(win2, text="Day 3 DC1 Initial")
-        lab10.grid(row=7, column=0)
-
-        lab11 = Label(win2, text="Day 3 DC1 Final")
-        lab11.grid(row=7, column=2)
-
-        lab12 = Label(win2, text="Day 3 DC2 Initial")
-        lab12.grid(row=8, column=0)
-
-        lab13 = Label(win2, text="Day 3 DC2 Final")
-        lab13.grid(row=8, column=2)
-
-        lab14 = Label(win2, text="Day 4 DC1 Initial")
-        lab14.grid(row=10, column=0)
-
-        lab15 = Label(win2, text="Day 4 DC1 Final")
-        lab15.grid(row=10, column=2)
-
-        lab16 = Label(win2, text="Day 4 DC2 Initial")
-        lab16.grid(row=11, column=0)
-
-        lab17 = Label(win2, text="Day Day 4 DC 2 Final")
-        lab17.grid(row=11, column=2)
-
-        lab18 = Label(win2, text="Day 2")
-        lab18.grid(row=3, column=0)
-
-        lab19 = Label(win2, text="Day 3")
-        lab19.grid(row=6, column=0)
-
-        lab20 = Label(win2, text="Day 4")
-        lab20.grid(row=9, column=0)
-
-        self.d1_dc_1_i_text = DoubleVar()
-        ent1 = Entry(win2, textvariable=self.d1_dc_1_i_text, width=5)
-        ent1.grid(row=1, column=1)
-
-        self.d1_dc_1_f_text = DoubleVar()
-        ent2 = Entry(win2, textvariable=self.d1_dc_1_f_text, width=5)
-        ent2.grid(row=1, column=3)
-
-        self.d1_dc_2_i_text = DoubleVar()
-        ent3 = Entry(win2, textvariable=self.d1_dc_2_i_text, width=5)
-        ent3.grid(row=2, column=1)
-
-        self.d1_dc_2_f_text = DoubleVar()
-        ent4 = Entry(win2, textvariable=self.d1_dc_2_f_text, width=5)
-        ent4.grid(row=2, column=3)
-
-        self.d2_dc_1_i_text = DoubleVar()
-        ent5 = Entry(win2, textvariable=self.d2_dc_1_i_text, width=5)
-        ent5.grid(row=4, column=1)
-
-        self.d2_dc_1_f_text = DoubleVar()
-        ent6 = Entry(win2, textvariable=self.d2_dc_1_f_text, width=5)
-        ent6.grid(row=4, column=3)
-
-        self.d2_dc_2_i_text = DoubleVar()
-        ent7 = Entry(win2, textvariable=self.d2_dc_2_i_text, width=5)
-        ent7.grid(row=5, column=1)
-
-        self.d2_dc_2_f_text = DoubleVar()
-        ent8 = Entry(win2, textvariable=self.d2_dc_2_f_text, width=5)
-        ent8.grid(row=5, column=3)
-
-        self.d3_dc_1_i_text = DoubleVar()
-        ent9 = Entry(win2, textvariable=self.d3_dc_1_i_text, width=5)
-        ent9.grid(row=7, column=1)
-
-        self.d3_dc_1_f_text = DoubleVar()
-        ent10 = Entry(win2, textvariable=self.d3_dc_1_f_text, width=5)
-        ent10.grid(row=7, column=3)
-
-        self.d3_dc_2_i_text = DoubleVar()
-        ent11 = Entry(win2, textvariable=self.d3_dc_2_i_text, width=5)
-        ent11.grid(row=8, column=1)
-
-        self.d3_dc_2_f_text = DoubleVar()
-        ent12 = Entry(win2, textvariable=self.d3_dc_2_f_text, width=5)
-        ent12.grid(row=8, column=3)
-
-        self.d4_dc_1_i_text = DoubleVar()
-        ent13 = Entry(win2, textvariable=self.d4_dc_1_i_text, width=5)
-        ent13.grid(row=10, column=1)
-
-        self.d4_dc_1_f_text = DoubleVar()
-        ent14 = Entry(win2, textvariable=self.d4_dc_1_f_text, width=5)
-        ent14.grid(row=10, column=3)
-
-        self.d4_dc_2_i_text = DoubleVar()
-        ent15 = Entry(win2, textvariable=self.d4_dc_2_i_text, width=5)
-        ent15.grid(row=11, column=1)
-
-        self.d4_dc_2_f_text = DoubleVar()
-        ent16 = Entry(win2, textvariable=self.d4_dc_2_f_text, width=5)
-        ent16.grid(row=11, column=3)
-
-        but1 = Button(win2, text="Save", width=12, command=DCMod.metadata_dc_list)
-        but1.grid(row=12, column=3)
-        but2 = Button(win2, text="Close", width=12)
-        but2.grid(row=13, column=3)
-
-        win2.mainloop()
-
-    def metadata_dc_list(self):
-        dc_list1 = [self.d1_dc_1_i_text.get(), self.d1_dc_1_f_text.get(), self.d1_dc_2_i_text.get(),
-                    self.d1_dc_2_f_text.get(), self.d2_dc_1_i_text.get(), self.d2_dc_1_f_text.get(),
-                    self.d2_dc_2_i_text.get(), self.d2_dc_2_f_text.get(), self.d3_dc_1_i_text.get(),
-                    self.d3_dc_1_f_text.get(), self.d3_dc_2_i_text.get(), self.d3_dc_2_f_text.get(),
-                    self.d4_dc_1_i_text.get(), self.d4_dc_1_f_text.get(), self.d4_dc_2_i_text.get(),
-                    self.d4_dc_2_f_text.get()]
-
-        diff_d1_dc1 = self.d1_dc_1_i_text.get() - self.d1_dc_1_f_text.get()
-        diff_d1_dc2 = self.d1_dc_2_i_text.get() - self.d1_dc_2_f_text.get()
-        diff_d2_dc1 = self.d2_dc_1_i_text.get() - self.d2_dc_1_f_text.get()
-        diff_d2_dc2 = self.d2_dc_2_i_text.get() - self.d2_dc_2_f_text.get()
-        diff_d3_dc1 = self.d3_dc_1_i_text.get() - self.d3_dc_1_f_text.get()
-        diff_d3_dc2 = self.d3_dc_2_i_text.get() - self.d3_dc_2_f_text.get()
-        diff_d4_dc1 = self.d4_dc_1_i_text.get() - self.d4_dc_1_f_text.get()
-        diff_d4_dc2 = self.d4_dc_2_i_text.get() - self.d4_dc_2_f_text.get()
-
-        dc_list2 = [diff_d1_dc1, diff_d1_dc2, diff_d2_dc1, diff_d2_dc2, diff_d3_dc1, diff_d3_dc2,
-                    diff_d4_dc1, diff_d4_dc2]
-
-        return dc_list1 + dc_list2
+def connect():
+    conn = sqlite3.connect("mouse_test4.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS mouse_test4(id INTEGER PRIMARY KEY, 'mouse ID' text, 'weight' float,"
+                "'Day 1 V Initial' float, 'Day 1 V Final' float, 'Day 1 DC1 Initial' float, 'Day 1 DC1 Final' float,"
+                "'Day 1 DC2 Initial' float, 'Day 1 DC2 Final' float, 'Day 2 V Initial' float, 'Day 2 V Final' float,"
+                "'Day 2 DC1 Initial' float, 'Day 2 DC1 Final' float, 'Day 2 DC2 Initial' float,"
+                "'Day 2 DC2 Final' float, 'Day 3 V Initial' float, 'Day 3 V Final' float, 'Day 3 DC1 Initial' float,"
+                "'Day 3 DC1 Final' float, 'Day 3 DC2 Initial' float, 'Day 3 DC2 Final' float, 'Day 4 V Initial' float,"
+                "'Day 4 V Final' float, 'Day 4 DC1 Initial' float, 'Day 4 DC1 Final' float, 'Day 4 DC2 Initial' float,"
+                "'Day 4 DC2 Final' float)")
+    conn.commit()
+    conn.close()
 
 
-
-
-
-
-
-
-
+def insert(mouseID, wgt, D1VI, D1VF, D1DC1I, D1DC1F, D1DC2I, D1DC2F, D2VI, D2VF, D2DC1I, D2DC1F, D2DC2I, D2DC2F,
+           D3VI, D3VF, D3DC1I, D3DC1F, D3DC2I, D3DC2F, D4VI, D4VF, D4DC1I, D4DC1F, D4DC2I, D4DC2F):
+    conn = sqlite3.connect("mouse_test4.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO mouse_test4 VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                (mouseID, wgt, D1VI, D1VF, D1DC1I, D1DC1F, D1DC2I, D1DC2F, D2VI, D2VF, D2DC1I, D2DC1F, D2DC2I, D2DC2F,
+                 D3VI, D3VF, D3DC1I, D3DC1F, D3DC2I, D3DC2F, D4VI, D4VF, D4DC1I, D4DC1F, D4DC2I, D4DC2F))
+    conn.commit()
+    conn.close()
 
--- frontend_did.py
+++ frontend_did.py
@@ -1,6 +1,41 @@
 from tkinter import *
-
 import backend_did
+
+'''FOR DATA TECHNICIAN TO MODIFY BELOW'''
+# --------------------------------------------------------------- #
+
+'''DAY 1 DRIP CONTROL'''
+day_1_dc1_initial = 2
+day_1_dc1_final = 4
+
+day_1_dc2_initial = 6
+day_1_dc2_final = 8
+
+'''DAY 2 DRIP CONTROL'''
+day_2_dc1_initial = 10
+day_2_dc1_final = 12
+
+day_2_dc2_initial = 14
+day_2_dc2_final = 16
+
+'''DAY 3 DRIP CONTROL'''
+day_3_dc1_initial = 18
+day_3_dc1_final = 20
+
+day_3_dc2_initial = 22
+day_3_dc2_final = 24
+
+'''DAY 4 DRIP CONTROL'''
+day_4_dc1_initial = 26
+day_4_dc1_final = 28
+
+day_4_dc2_initial = 30
+day_4_dc2_final = 32
+
+# ----------------------------------------------------------------- #
+
+
+
 
 
 class MouseHouse:
@@ -35,7 +70,7 @@ class MouseHouse:
         l9.grid(row=8, column=0)
 
         l10 = Label(win, text="Day 4 Final")
-        l10.grid(row=8, column=0)
+        l10.grid(row=9, column=0)
 
         # TODO: Have tkinter pull last values from mouse database and put them as default entries
 
@@ -88,13 +123,13 @@ class MouseHouse:
         list1.configure(yscrollcommand=sb1.set)
         sb1.configure(command=list1.yview)
 
-        b7 = Button(win, text="Modify DC", width=12, command=backend_did.metadata_dc_list)
+        b7 = Button(win, text="Modify DC", width=12)
         b7.grid(row=7, column=2)
 
         b1 = Button(win, text="View all", width=12)
         b1.grid(row=8, column=2)
 
-        b2 = Button(win, text="Add Mouse", width=12)
+        b2 = Button(win, text="Add Mouse", width=12, command=self.add_command)
         b2.grid(row=9, column=2)
 
         b3 = Button(win, text="Delete Mouse", width=12)
@@ -109,23 +144,230 @@ class MouseHouse:
         b6 = Button(win, text="Close", width=12)
         b6.grid(row=10, column=3)
 
+        e1.bind("<Down>", self.next_widget)
+        e2.bind("<Down>", self.next_widget)
+        e3.bind("<Down>", self.next_widget)
+        e4.bind("<Down>", self.next_widget)
+        e5.bind("<Down>", self.next_widget)
+        e6.bind("<Down>", self.next_widget)
+        e7.bind("<Down>", self.next_widget)
+        e8.bind("<Down>", self.next_widget)
+        e1.bind("<Down>", self.next_widget)
+        e2.bind("<Down>", self.next_widget)
+
         win.mainloop()
 
+    def next_widget(self, event):
+        event.widget.tk_focusNext().focus()
+        return "break"
+
+    def add_command(self):
+        backend_did.insert(self.mouse_number_text.get(), self.weight_text.get(), self.d1_vi_text.get(),
+                           self.d1vf_text.get(), day_1_dc1_initial, day_1_dc1_final, day_1_dc2_initial,
+                           day_1_dc2_final, self.d2_vi_text.get(), self.d2_vf_text.get(), day_2_dc1_initial,
+                           day_2_dc1_final, day_2_dc2_initial, day_2_dc2_final, self.d3_vi_text.get(),
+                           self.d3_vf_text.get(), day_3_dc1_initial, day_3_dc1_final, day_3_dc2_initial,
+                           day_3_dc2_final, self.d4_vi_text.get(), self.d4_vf_text.get(), day_4_dc1_initial,
+                           day_4_dc1_final, day_4_dc2_initial, day_4_dc2_final)
+
     def meta_data_v_list(self):
-        list_v = [self.d1_vi_text.get(), self.d1vf_text.get(),
+        mouse_number = self.mouse_number_text
+        wgt = self.weight_text
+        list_v = [mouse_number, wgt, self.d1_vi_text.get(), self.d1vf_text.get(),
                   self.d2_vi_text.get(), self.d2_vf_text.get(),
-                  self.d3_vi_text.get(),self.d3_vf_text.get(),
+                  self.d3_vi_text.get(), self.d3_vf_text.get(),
                   self.d4_vi_text.get(), self.d4_vf_text.get()]
-        return list_v
+        return print(list_v)
 
     def meta_data_v_diff_list(self):
+        # difference in final and initial
         diff_d1_v = self.d1_vi_text.get() - self.d1vf_text.get()
         diff_d2_v = self.d2_vi_text.get() - self.d2_vf_text.get()
         diff_d3_v = self.d3_vi_text.get() - self.d3_vf_text.get()
         diff_d4_v = self.d4_vi_text.get() - self.d4_vf_text.get()
-        # TODO: Combine with DC metadata
-        list_v_diff = [diff_d1_v, diff_d2_v, diff_d2_v, diff_d3_v, diff_d4_v]
-        return print(list_v_diff)
+
+        # Average in difference across days
+        avg_diff_v = (diff_d1_v + diff_d2_v + diff_d3_v + diff_d4_v)/4
+
+        list_v1 = [diff_d1_v, diff_d2_v, diff_d2_v, diff_d3_v, diff_d4_v, avg_diff_v]
+        return print(list_v1)
+
+# -------------------------------------------------------- #
+
+
+# class DClist:
+#
+#     def __init__(self):
+#         win2 = Tk()
+#
+#         lab1 = Label(win2, text="Day 1")
+#         lab1.grid(row=0, column=0)
+#
+#         lab2 = Label(win2, text="Day 1 DC1 Initial")
+#         lab2.grid(row=1, column=0)
+#
+#         lab3 = Label(win2, text="Day 1 DC1 Final")
+#         lab3.grid(row=1, column=2)
+#
+#         lab4 = Label(win2, text="Day 1 DC2 Initial")
+#         lab4.grid(row=2, column=0)
+#
+#         lab5 = Label(win2, text="Day 1 DC2 Final")
+#         lab5.grid(row=2, column=2)
+#
+#         lab6 = Label(win2, text="Day 2 DC1 Initial")
+#         lab6.grid(row=4, column=0)
+#
+#         lab7 = Label(win2, text="Day 2 DC1 Final")
+#         lab7.grid(row=4, column=2)
+#
+#         lab8 = Label(win2, text="Day 2 DC2 Initial")
+#         lab8.grid(row=5, column=0)
+#
+#         lab9 = Label(win2, text="Day 2 DC2 Final")
+#         lab9.grid(row=5, column=2)
+#
+#         lab10 = Label(win2, text="Day 3 DC1 Initial")
+#         lab10.grid(row=7, column=0)
+#
+#         lab11 = Label(win2, text="Day 3 DC1 Final")
+#         lab11.grid(row=7, column=2)
+#
+#         lab12 = Label(win2, text="Day 3 DC2 Initial")
+#         lab12.grid(row=8, column=0)
+#
+#         lab13 = Label(win2, text="Day 3 DC2 Final")
+#         lab13.grid(row=8, column=2)
+#
+#         lab14 = Label(win2, text="Day 4 DC1 Initial")
+#         lab14.grid(row=10, column=0)
+#
+#         lab15 = Label(win2, text="Day 4 DC1 Final")
+#         lab15.grid(row=10, column=2)
+#
+#         lab16 = Label(win2, text="Day 4 DC2 Initial")
+#         lab16.grid(row=11, column=0)
+#
+#         lab17 = Label(win2, text="Day Day 4 DC 2 Final")
+#         lab17.grid(row=11, column=2)
+#
+#         lab18 = Label(win2, text="Day 2")
+#         lab18.grid(row=3, column=0)
+#
+#         lab19 = Label(win2, text="Day 3")
+#         lab19.grid(row=6, column=0)
+#
+#         lab20 = Label(win2, text="Day 4")
+#         lab20.grid(row=9, column=0)
+#
+#         self.d1_dc_1_i_text = DoubleVar()
+#         ent1 = Entry(win2, textvariable=self.d1_dc_1_i_text, width=5)
+#         ent1.grid(row=1, column=1)
+#
+#         self.d1_dc_1_f_text = DoubleVar()
+#         ent2 = Entry(win2, textvariable=self.d1_dc_1_f_text, width=5)
+#         ent2.grid(row=1, column=3)
+#
+#         self.d1_dc_2_i_text = DoubleVar()
+#         ent3 = Entry(win2, textvariable=self.d1_dc_2_i_text, width=5)
+#         ent3.grid(row=2, column=1)
+#
+#         self.d1_dc_2_f_text = DoubleVar()
+#         ent4 = Entry(win2, textvariable=self.d1_dc_2_f_text, width=5)
+#         ent4.grid(row=2, column=3)
+#
+#         self.d2_dc_1_i_text = DoubleVar()
+#         ent5 = Entry(win2, textvariable=self.d2_dc_1_i_text, width=5)
+#         ent5.grid(row=4, column=1)
+#
+#         self.d2_dc_1_f_text = DoubleVar()
+#         ent6 = Entry(win2, textvariable=self.d2_dc_1_f_text, width=5)
+#         ent6.grid(row=4, column=3)
+#
+#         self.d2_dc_2_i_text = DoubleVar()
+#         ent7 = Entry(win2, textvariable=self.d2_dc_2_i_text, width=5)
+#         ent7.grid(row=5, column=1)
+#
+#         self.d2_dc_2_f_text = DoubleVar()
+#         ent8 = Entry(win2, textvariable=self.d2_dc_2_f_text, width=5)
+#         ent8.grid(row=5, column=3)
+#
+#         self.d3_dc_1_i_text = DoubleVar()
+#         ent9 = Entry(win2, textvariable=self.d3_dc_1_i_text, width=5)
+#         ent9.grid(row=7, column=1)
+#
+#         self.d3_dc_1_f_text = DoubleVar()
+#         ent10 = Entry(win2, textvariable=self.d3_dc_1_f_text, width=5)
+#         ent10.grid(row=7, column=3)
+#
+#         self.d3_dc_2_i_text = DoubleVar()
+#         ent11 = Entry(win2, textvariable=self.d3_dc_2_i_text, width=5)
+#         ent11.grid(row=8, column=1)
+#
+#         self.d3_dc_2_f_text = DoubleVar()
+#         ent12 = Entry(win2, textvariable=self.d3_dc_2_f_text, width=5)
+#         ent12.grid(row=8, column=3)
+#
+#         self.d4_dc_1_i_text = DoubleVar()
+#         ent13 = Entry(win2, textvariable=self.d4_dc_1_i_text, width=5)
+#         ent13.grid(row=10, column=1)
+#
+#         self.d4_dc_1_f_text = DoubleVar()
+#         ent14 = Entry(win2, textvariable=self.d4_dc_1_f_text, width=5)
+#         ent14.grid(row=10, column=3)
+#
+#         self.d4_dc_2_i_text = DoubleVar()
+#         ent15 = Entry(win2, textvariable=self.d4_dc_2_i_text, width=5)
+#         ent15.grid(row=11, column=1)
+#
+#         self.d4_dc_2_f_text = DoubleVar()
+#         ent16 = Entry(win2, textvariable=self.d4_dc_2_f_text, width=5)
+#         ent16.grid(row=11, column=3)
+#
+#         but1 = Button(win2, text="Save", width=12)
+#         but1.grid(row=12, column=3)
+#         but2 = Button(win2, text="Close", width=12)
+#         but2.grid(row=13, column=3)
+#
+#         win2.mainloop()
+#
+#     def dc_data(self):
+#         dc_list1 = [self.d1_dc_1_i_text.get(), self.d1_dc_1_f_text.get(), self.d1_dc_2_i_text.get(),
+#                     self.d1_dc_2_f_text.get(), self.d2_dc_1_i_text.get(), self.d2_dc_1_f_text.get(),
+#                     self.d2_dc_2_i_text.get(), self.d2_dc_2_f_text.get(), self.d3_dc_1_i_text.get(),
+#                     self.d3_dc_1_f_text.get(), self.d3_dc_2_i_text.get(), self.d3_dc_2_f_text.get(),
+#                     self.d4_dc_1_i_text.get(), self.d4_dc_1_f_text.get(), self.d4_dc_2_i_text.get(),
+#                     self.d4_dc_2_f_text.get()]
+#
+#         # Difference in DC final and initial
+#         diff_d1_dc1 = self.d1_dc_1_i_text.get() - self.d1_dc_1_f_text.get()
+#         diff_d1_dc2 = self.d1_dc_2_i_text.get() - self.d1_dc_2_f_text.get()
+#         diff_d2_dc1 = self.d2_dc_1_i_text.get() - self.d2_dc_1_f_text.get()
+#         diff_d2_dc2 = self.d2_dc_2_i_text.get() - self.d2_dc_2_f_text.get()
+#         diff_d3_dc1 = self.d3_dc_1_i_text.get() - self.d3_dc_1_f_text.get()
+#         diff_d3_dc2 = self.d3_dc_2_i_text.get() - self.d3_dc_2_f_text.get()
+#         diff_d4_dc1 = self.d4_dc_1_i_text.get() - self.d4_dc_1_f_text.get()
+#         diff_d4_dc2 = self.d4_dc_2_i_text.get() - self.d4_dc_2_f_text.get()
+#
+#         dc_diff_list = [diff_d1_dc1, diff_d1_dc2, diff_d2_dc1, diff_d2_dc2, diff_d3_dc1, diff_d3_dc2,
+#                         diff_d4_dc1, diff_d4_dc2]
+#
+#         dc_lists = print(dc_list1 + dc_diff_list)
+#         return dc_lists
+#         # Average DC for each day
+#
+#         global avg_dc_d1
+#         global avg_dc_d2
+#         global avg_dc_d3
+#         global avg_dc_d4
+#         avg_dc_d1 = (diff_d1_dc1 + diff_d1_dc2)/2
+#         avg_dc_d2 = (diff_d2_dc1 + diff_d2_dc2)/2
+#         avg_dc_d3 = (diff_d3_dc1 + diff_d3_dc2)/2
+#         avg_dc_d4 = (diff_d4_dc1 + diff_d4_dc2)/2
+#
+#
+#
+#
 
 
 # TODO: create database and table (easy) importance: 3
@@ -138,4 +380,5 @@ class MouseHouse:
 # TODO: Make Update function (medium) Importance: 1
 # TODO: Have program pull last saved data on program initialization (hard) Importance: 4
 # TODO: Make formulas for program to churn (medium) Importance: 6
+backend_did.connect()
 MouseHouse()
